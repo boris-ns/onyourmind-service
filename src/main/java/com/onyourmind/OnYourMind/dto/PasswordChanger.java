cf055a7b1190e58e9c81149a64cc0134034d8dc2
Boris Sulicenko <b.sulicenko@gmail.com>
2019-10-04 22:16:10 +0200

    Refactored AuthenticationController. Deleted unnecessary files.

---
+++ src/main/java/com/onyourmind/OnYourMind/dto/PasswordChanger.java
@@ -0,0 +1,26 @@
+package com.onyourmind.OnYourMind.dto;
+
+public class PasswordChanger {
+
+    private String oldPassword;
+    private String newPassword;
+
+    public PasswordChanger() {
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
--- src/main/java/com/onyourmind/OnYourMind/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/onyourmind/OnYourMind/service/impl/CustomUserDetailsService.java
@@ -1,12 +1,17 @@
 package com.onyourmind.OnYourMind.service.impl;
 
 import com.onyourmind.OnYourMind.dto.UserDTO;
+import com.onyourmind.OnYourMind.exception.ApiRequestException;
 import com.onyourmind.OnYourMind.model.User;
+import com.onyourmind.OnYourMind.model.UserTokenState;
 import com.onyourmind.OnYourMind.repository.UserRepository;
+import com.onyourmind.OnYourMind.security.TokenUtils;
+import com.onyourmind.OnYourMind.security.auth.JwtAuthenticationRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +20,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -29,6 +36,10 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenUtils tokenUtils;
+
 
     /* Return User from database */
     @Override
@@ -61,4 +72,43 @@ public class CustomUserDetailsService implements UserDetailsService {
         userRepository.save(user);
     }
 
+    public UserDTO login(JwtAuthenticationRequest authenticationRequest) {
+
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(
+                            authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new ApiRequestException("Credentials are not valid!");
+        }
+
+        // Insert username and password into context
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Create token
+        User user = (User) authentication.getPrincipal();
+        String jwt = tokenUtils.generateToken(user.getUsername());
+        int expiresIn = tokenUtils.getExpiredIn();
+
+        UserDTO userDto = new UserDTO(user);
+        userDto.setToken(new UserTokenState(jwt, expiresIn));
+
+        return userDto;
+    }
+
+    public UserTokenState refreshAuthenticationToken(HttpServletRequest request) throws ApiRequestException {
+        String token = tokenUtils.getToken(request);
+        String username = tokenUtils.getUsernameFromToken(token);
+        User user = (User) loadUserByUsername(username);
+
+        if (this.tokenUtils.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
+            String refreshedToken = tokenUtils.refreshToken(token);
+            int expiresIn = tokenUtils.getExpiredIn();
+            return new UserTokenState(refreshedToken, expiresIn);
+        } else {
+            throw new ApiRequestException("Token can not be refreshed.");
+        }
+    }
 }
